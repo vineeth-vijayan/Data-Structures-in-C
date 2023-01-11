f476f7cfd699a305f0395543e1a58fba40034f8f
Vineeth Kumar <vineethkumar6469@gmail.com>
2023-01-11 09:56:44 +0530

    Add files via upload

--- stack/balanced_brackets.c
+++ stack/balanced_brackets.c
@@ -5,29 +5,36 @@
 
 struct Brackets{
 	int top;
-	int size;
+	unsigned size;
 	char *ch;
 };
 
-void push(struct Brackets *b, char c){
-	//printf("push");
-	b->ch[++b->top]=c;
-	//printf("%d\n", b->top);
-	for(int i=0;i<=b->top; i++)
-		printf("%c ", b->ch[i]);
-	printf("\n");
+
+struct Brackets* createStack(unsigned s){
+	struct Brackets* b_stack = (struct Brackets*) malloc(sizeof(struct Brackets));
+
+	if(!b_stack)
+		return NULL;
+	b_stack->top=-1;
+	b_stack->size = s;
+	b_stack->ch = (char*)malloc(s*sizeof(char));
+
+	return b_stack;
 }
 
-char* pop(struct Brackets *b){
-	//printf("pop");	
-	//printf("%d\n", b->top);
-	char t;
+void push(struct Brackets *b, char c){
+	b->ch[++b->top]=c;
+}
+
+
+
+char pop(struct Brackets *b){
 	if(b->top>-1){
-		//printf("%c", b->ch[b->top]);
-		return &b->ch[b->top--];
+		//printf("pop: %c\n", b->ch[b->top]);
+		return b->ch[b->top--];
 	}
 	else{
-		printf("1.No, brackets are not balanced!!");
+		printf("No\n");
 		exit(0);
 	}
 }
@@ -40,47 +47,33 @@ bool isEmpty(struct Brackets *b){
 		return 0;
 }
 
+bool compare(char a, char b){
+	//printf("compare: (%c:%c)\n", a, b);
+	char *beta = &b, *alpha =  &a;
+	return strcmp(beta,")") && strcmp(alpha,"(") || strcmp(alpha,"{") && strcmp(beta,"}") || strcmp(alpha,"[") && strcmp(beta,"]");
+}
+
 bool bracket_balanced(char *c){
-	char* t;
-	struct Brackets b;
-	b.top = -1;
-	b.size = 20;
-	b.ch = (char*)malloc(20);
+	char t;
+	struct Brackets *b = createStack(20);
+	
 	for(int i=0; i<strlen(c); i++){
-		//printf("\n%c\n", c[i]);
 		switch(c[i]){
 			case '{':
-				push(&b, c[i]);
-				break;
 			case '[':
-				push(&b, c[i]);
-				break;
 			case '(':
-				push(&b, c[i]);
+				push(b, c[i]);
 				break;
 			case ')':
-				t = pop(&b);
-				printf("%d ", strcmp(t,"("));
-				if(strcmp(t,"(")!=0)
-					return 0;
-				break;
 			case '}':
-				t = pop(&b);
-				printf("%d " , strcmp(t,"{"));
-				if(strcmp(t,"{")!=0)
-					return 0;
-				break;
 			case ']':
-				t = pop(&b);
-				printf("%d ", strcmp(t,"["));	
-				if(strcmp(t, "[")!=0)
+				t = pop(b);
+				if(!compare(t, c[i]))
 					return 0;
 				break;
-			default:
-				continue;
 		}
 	}
-	if(isEmpty(&b))
+	if(isEmpty(b))
 		return 1;
 	else
 		return 0;
@@ -88,15 +81,14 @@ bool bracket_balanced(char *c){
 
 
 int main(){
-	char *chr;
-	chr = (char*) malloc(20);	
-	
-	printf("Read brackets: ");
-	scanf("%s", chr);
-	if(bracket_balanced(chr))
-		printf("Yes");
+	char *c;
+	printf("Enter string of brackets: ");
+	c = (char*)malloc(20*sizeof(char));
+	scanf("%s", c);
+	if(bracket_balanced(c))
+		printf("Yes\n");
 	else
-		printf("No");
+		printf("No\n");
 
 	return 0;
 }
