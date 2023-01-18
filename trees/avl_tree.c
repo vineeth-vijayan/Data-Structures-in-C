256cfc0a704a735f1c83627cd6f1af4d66c7d5e6
Vineeth Kumar <vineethkumar6469@gmail.com>
2023-01-18 09:37:04 +0530

    Add files via upload

--- trees/avl_tree.c
+++ trees/avl_tree.c
@@ -3,7 +3,7 @@
 
 struct AVL
 {
-	int key;
+	int data;
 	struct AVL *left;
 	struct AVL *right;
 	int height;
@@ -21,10 +21,10 @@ int max(int a, int b)
 	return (a > b)? a : b;
 }
 
-struct AVL* newAVL(int key)
+struct AVL* newNode(int data)
 {
 	struct AVL* node = (struct AVL*)malloc(sizeof(struct AVL));
-	node->key = key;
+	node->data = data;
 	node->left = NULL;
 	node->right = NULL;
 	node->height = 1;
@@ -66,15 +66,15 @@ int getBalance(struct AVL *N)
 	return height(N->left) - height(N->right);
 }
 
-struct AVL* insert(struct AVL* node, int key)
+struct AVL* insert(struct AVL* node, int data)
 {
 	if (node == NULL)
-		return(newAVL(key));
+		return(newNode(data));
 
-	if (key < node->key)
-		node->left = insert(node->left, key);
-	else if (key > node->key)
-		node->right = insert(node->right, key);
+	if (data < node->data)
+		node->left = insert(node->left, data);
+	else if (data > node->data)
+		node->right = insert(node->right, data);
 	else 
 		return node;
 
@@ -83,22 +83,22 @@ struct AVL* insert(struct AVL* node, int key)
 	int balance = getBalance(node);
 
 	// Left Left Case
-	if (balance > 1 && key < node->left->key)
+	if (balance > 1 && data < node->left->data)
 		return rightRotate(node);
 
 	// Right Right Case
-	if (balance < -1 && key > node->right->key)
+	if (balance < -1 && data > node->right->data)
 		return leftRotate(node);
 
 	// Left Right Case
-	if (balance > 1 && key > node->left->key)
+	if (balance > 1 && data > node->left->data)
 	{
 		node->left = leftRotate(node->left);
 		return rightRotate(node);
 	}
 
 	// Right Left Case
-	if (balance < -1 && key < node->right->key)
+	if (balance < -1 && data < node->right->data)
 	{
 		node->right = rightRotate(node->right);
 		return leftRotate(node);
@@ -107,11 +107,62 @@ struct AVL* insert(struct AVL* node, int key)
 	return node;
 }
 
+struct AVL *maxValue(struct AVL *node){
+	while(node && node->right!=NULL)
+		node = node->right;
+	return node;
+}
+
+struct AVL *delete(struct AVL *node, int data){
+	if(node == NULL)
+		return node;
+	
+	if(node->data > data)
+		node->left = delete(node->left, data);
+	else if(node->data < data)
+		node->right = delete(node->right, data);
+	else{
+		if((node->left==NULL) || (node->right==NULL)){
+			struct AVL *temp = node->left ? node->left:node->right;
+			if(temp == NULL)
+				node = NULL;
+			else
+				*node = *temp;
+		}
+		else{
+			struct AVL *temp = maxValue(node->left);
+			node->data = temp->data;
+			node->left = delete(node->left, temp->data);
+		}
+	}
+
+	if(node == NULL)
+		return node;
+
+	node->height = 1 + max(height(node->left), height(node->right));
+
+	int balance = getBalance(node);
+
+	if(balance>1 && getBalance(node->left)>=0)
+		return leftRotate(node);
+	if(balance>1 && getBalance(node->left)<0){
+		node->left = leftRotate(node->left);
+		return rightRotate(node);
+	}
+	if(balance<-1 && getBalance(node->right)<=0)
+		return rightRotate(node);
+	if(balance<-1 && getBalance(node->right)>0){
+		node->right = rightRotate(node->right);
+		return leftRotate(node);
+	}
+	return node;
+}
+
 void preOrder(struct AVL *root)
 {
 	if(root != NULL)
 	{
-		printf("%d ", root->key);
+		printf("%d ", root->data);
 		preOrder(root->left);
 		preOrder(root->right);
 	}
@@ -120,21 +171,26 @@ void preOrder(struct AVL *root)
 int main()
 {
 	struct AVL *root = NULL;
-	root = insert(root, 100);
-	root = insert(root, 150);
-	root = insert(root, 80);
+	
+	//Insert
+	root = insert(root, 10);
+	root = insert(root, 20);
+	root = insert(root, 30);
+	root = insert(root, 40);
+	root = insert(root, 50);
 	root = insert(root, 60);
-	root = insert(root, 25);
-	root = insert(root, 200);
-	root = insert(root, 120);
-	root = insert(root, 130);
-	root = insert(root, 170);
-	root = insert(root, 90);
-	root = insert(root, 75);
+	root = insert(root, 70);
+	root = insert(root, 80);
 
+	//Delete
+	root = delete(root, 70);
+	root = delete(root, 60);
+	root = delete(root, 40);
+	root = delete(root, 20);
 
 	printf("\tPreorder traversal: ");
 	preOrder(root);
+	printf("\n");
 
 	return 0;
 }
